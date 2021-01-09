61dfe05caf4e4509c7aac42b3a7a928012a6330b
nights-demons <75934009+nights-demons@users.noreply.github.com>
2021-01-09 12:55:00 +0400

    Add files via upload

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+print("Обновление pip...")
+os.system("pip3 install --upgrade pip")
+os.system("clear")
+print("              [Installer]\n")
+mod = ["requests", "bs4"]
+input("Нажмите Enter, для установки нужных библиотек.")
+print("Начало установки.")
+for i in range(len(mod)):
+    print("Установка "+mod[i]+"...")
+    os.system("pip3 install "+mod[i])
